e8d04200d78b03f07511efb4686b2fd6012586aa
Rodrigo Alviani <rodrigoalviani@gmail.com>
2014-12-11 14:41:00 -0200

    Initial version

---
+++ src/cache.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rodrigoalviani\Cache;
+
+/*
+ * PHP Simple Cache v0.1.0
+ *
+ * By Rodrigo Alviani
+ * http://github.com/rodrigoalviani
+ *
+ * MIT license
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+class Cache {
+}
